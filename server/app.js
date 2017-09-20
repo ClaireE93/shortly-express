546fbe97518d785a0a32fe462076332c831d5310
Claire Johnson <c.eliseee@gmail.com>
2017-09-20 12:31:33 -0700

    Working on add user middleware

--- server/app.js
+++ server/app.js
@@ -6,7 +6,9 @@ const bodyParser = require('body-parser');
 const Auth = require('./middleware/auth');
 const models = require('./models');
 const mysql = require('mysql');
-const db = require('./db');
+// const db = require('./db');
+const Promise = require('bluebird');
+const db = Promise.promisifyAll(require('./db'));
 
 const app = express();
 
@@ -79,28 +81,7 @@ app.post('/links',
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
-app.post('/signup', (req, res, next) => {
-  const inputPass = req.body.password;
-  const newSalt = utils.createRandom32String();
-  const newPass = utils.createHash(inputPass, newSalt);
-  // const queryString = `INSERT INTO users (username, password, salt) VALUES ("${newUser}", "${newPass}", "${newSalt}")`;
-  const queryString = `INSERT INTO users SET ?`;
-  const newUser = {
-    username: req.body.username,
-    password: newPass,
-    salt: newSalt,
-  };
-  db.query(queryString, newUser, function(err, results) {
-    if (err) {
-      res.statusCode = 400;
-      res.end(err.toString());
-    } else {
-      res.statusCode = 200;
-      console.log('ending response', results);
-      res.end();
-    }
-  });
-});
+app.post('/signup', Auth.addUser);
 
 
 /************************************************************/
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,10 +1,53 @@
 const models = require('../models');
 const Promise = require('bluebird');
+const mysql = require('mysql');
+const db = Promise.promisifyAll(require('../db'));
+const utils = require('../lib/hashUtils');
 
 module.exports.createSession = (req, res, next) => {
 };
 
+const userExists = (username) => {
+  const queryString = `SELECT * FROM users WHERE username = "${username}"`;
+  db.queryAsync(queryString)
+  .then((results) => {
+    if (results.length > 0) {
+      console.log('returning true');
+      return true;
+    } else {
+      return false;
+    }
+  })
+  .catch((err) => {
+    console.error('ERROR in userExists', err);
+    return false;
+  });
+};
+
+module.exports.addUser = (req, res, next) => {
+  const inputPass = req.body.password;
+  const newSalt = utils.createRandom32String();
+  const newPass = utils.createHash(inputPass, newSalt);
+  const queryString = 'INSERT INTO users SET ?';
+  const newUser = {
+    username: req.body.username,
+    password: newPass,
+    salt: newSalt,
+  };
+  db.queryAsync(queryString, newUser)
+  .then((results) => {
+    console.log('returning!');
+    res.statusCode = 200;
+    res.end();
+  })
+  .catch((err) => {
+    res.statusCode = 400;
+    res.end(err.toString());
+  });
+};
+
+
+
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
-
